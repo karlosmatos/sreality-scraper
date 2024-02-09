7659ea6b807e00e6b5a3d181100934e8cb1c583f
Karel Malý <105454484+karlosmatos@users.noreply.github.com>
2024-02-09 23:20:29 +0100

    Enable SrealityPipeline in item pipelines

--- sreality/settings.py
+++ sreality/settings.py
@@ -73,9 +73,9 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "sreality.pipelines.SrealityPipeline": 300,
-#}
+ITEM_PIPELINES = {
+    "sreality.pipelines.SrealityPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
