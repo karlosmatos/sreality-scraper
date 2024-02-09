d720a0fb51b16c02b6744e2a1193dc3f5fb8789c
Karel Malý <105454484+karlosmatos@users.noreply.github.com>
2024-02-09 18:27:57 +0100

    Refactor SrealitySpider to paginate through API results

--- sreality/spiders/sreality_spider.py
+++ sreality/spiders/sreality_spider.py
@@ -4,28 +4,22 @@ import json
 class SrealitySpider(scrapy.Spider):
     name = "sreality"
     allowed_domains = ["www.sreality.cz"]
-    
-    # Define the start URL and the query parameters directly in the URL
-    start_urls = [
-        "https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&locality_region_id=10&per_page=999"
-    ]
+    base_url = "https://www.sreality.cz/api/cs/v2/estates"
+    per_page = 999  # Number of items per page
+
+    def start_requests(self):
+        url = f"{self.base_url}?per_page={self.per_page}"
+        yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
-        # Convert the response data to JSON
         data = json.loads(response.text)
-        
-        # Save the JSON data to a file
-        with open('response.json', 'w') as f:
-            json.dump(data, f, indent=4)
-        
-        # If you want to process the data further, you can iterate over the items
-        # For example, to yield each estate's basic info:
-        for estate in data.get('_embedded', {}).get('estates', []):
-            yield {
-                'id': estate.get('hash_id'),
-                'name': estate.get('name'),
-                'price': estate.get('price_czk'),
-                'locality': estate.get('locality_label'),
-                'category': estate.get('category_main_cb_label'),
-                'url': estate.get('_links', {}).get('self', {}).get('href'),
-            }
+        result_size = data.get('result_size')
+        total_pages = result_size // self.per_page + 2
+
+        for page in range(1, total_pages):
+            url = f"{self.base_url}?per_page={self.per_page}&page={page}"
+            yield scrapy.Request(url, callback=self.parse_estate)
+
+    def parse_estate(self, response):
+        data = json.loads(response.text)
+        yield from data.get('_embedded', {}).get('estates', [])
